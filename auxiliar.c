274f23e020e4c1b0f18684bb34b16fd0d19ed0ec
Alejo García Galán <alejogarciagalan@gmail.com>
2020-07-09 20:10:47 +0200

    El programa principal esta casi terminado, en el header se han añadido unas cuantas funciones más y se ha creado el fichero de codigo fuente para dichas funciones, aun vacio. Hay que pensar que funciones faltan en el auxiliar.h antes de empezar auxiliar.c

--- auxiliar.c
+++ auxiliar.c
@@ -1,57 +1,25 @@
+// Condigo fuente del fichero auxiliar.h
+
 // Header que contiene las funciones auxiliares para el programa de la calculadora
 
-
 // Comprobamos que no se haya importado antes para no importarlo dos veces
-
 #ifndf AUXILIAR_H
 #define AUXILIAR_H
 
-
-
 // Cadenas que se emplearán en la interfaz de la calculadora de forma recurrente
-
-#define TEXTOMENU "Calculadora Binaria Simple\n"
-#define TEXTOMENU "1.- OR lógico\n2.- AND lógico\n3.- XOR lógico\n4.- << (desplazamiento a la izquierda)\n5.- >> (desplazamiento a la derecha)\n6.- Cambio de formato (0→ binario, 1→ hexadecimal)\n7.- Habilitar memoria\n8.- Deshabilitar memoria\n9.- Borrar memoria\n10.- Mostrar el contenido de la memoria\n0.- SALIR"
+#define TEXTOMENU "Calcula"
 #define BARRA ------------------------------------------------------
-
-
 
 //----------------------------------------------------------------------------------
 // Estructura para implementar la memoria de la calculadora (dada por el ejercicio)
 //----------------------------------------------------------------------------------
 
-typedef struct node{
-    union {
-        int dato; // operando o resultado para hexadecimal
-        char dato_binario[9]; //cadena para almacenar el operando/resultado binario
-        char operacion[30]; // cadena descriptiva de la operacion
-    } contenido;
-    void * siguiente;
-} node;
-
-
-
-//-----------------------------------------------
-// Funciones de entrada/salida de la calculadora
-//-----------------------------------------------
-
-void mostrarTitulo();   // Muestra la cabecera del programa
-
-void mostrarMenu();     // Muestra el menu inicial del programa
-
-int leeValor(char*, char*, int, int);   // Lee un valor numérico del teclado entre dos cotas dadas pidiendolo 
-                                        // con el mensaje indicado, en caso de salirse del margen imprime el
-                                        // mensaje de error que se le indica
-
-
-
 //-------------------------------------------------------------
 // Funciones de ejecución de las operaciones de la calculadora
 //-------------------------------------------------------------
 
-int ejecutar(char, int, int);
 
-void aBinario(int,**char);
+
 
 
 //-----------------------------------------------------
@@ -66,7 +34,8 @@ de cada operación). Todos los datos numericos se guardarán en el formato en el
 
 */
 
-void guarda(nodo**,char,int,int,int);    // Guarda un elemento completo en la calculadora del tipo:
+
+void guarda(nodo**);    // Guarda un elemento completo en la calculadora del tipo:
                         // [operación] [argumento/s(opcional)]
 
 void borra(nodo**);     // Función auxiliar para borraMemoria(), borra el último que se haya guardado en memoria
@@ -80,7 +49,6 @@ void borraMemoria(nodo**);  // Borra el contenido de la memoria al completo deja
 
 void imprimeMemoria(nodo*); // Imprime de forma consecutiva todos los elementos contenidos en memoria hasta la ultima
                             // entrada (sig == NULL). En caso de recibir memoria vacia escribe la cadena "MEMORIA VACIA"
-
 
                            
 #endif // Se cierra el if del preprocesador que comprueba si esta libreria ya estaba cargada
--- calculadora.c
+++ calculadora.c
@@ -1,5 +1,96 @@
 #include <stdio.h>
+#include "auxiliar.h"
+
 
 int main (void){
-    printf("Hello World");
+    
+    // Definición de las 5 variables del programa:
+    
+    unsigned char estado = 207;
+        // estado es una variable de 8 bits formada por los siguientes flags:
+            // bit 7: memoria habilitada (0 no, 1 si)
+            // bit 6: formato (0 binario, 1 hexadecimal)
+            // bit 5: ¿salir? (0 no, 1 el programa acaba en la siguiente iteración)
+            // bit 4: sin uso
+            // bits 3 a 0: codificación de la operación seleccionada siguiendo la
+                // numeración del menú 
+    
+    int a, b, r;    
+        // a, b y r funcionan como registros de operación 
+        // (a es primer operando, b el segundo y r almacena el resultado) 
+
+    node * memoria = NULL;   
+        // memoria es un puntero a la estructura node sobre la que se implementa 
+        // la memoria dinámica del programa (cadena) se inicialica vacia (NULL)
+
+    
+    // La primera función a la que se llama es la que imprime por pantalla el
+    // título del programa (solo se hace una vez en toda la ejecución)
+    mostrarTitulo();
+    
+    
+    // Mientras el flag "¿Salir?" este a 0 en la variable estado ejecutamos el 
+    // bucle principal del programa
+    while(!(estado&32){
+        
+        // Limpiamos la variable del estado poniendola a 0 (operación SALIR)
+        estado &= ~15;
+        
+        // Mostramos el menú del programa al usuario
+        mostrarMenu();
+        
+        // Pedimos el código de la operación a realizar
+        a = leeValor("Seleccione una acción: ","El valor introducido no es correcto\n",0,10);
+        
+        // Actualizamos el valor del codigo de operacion en estado con el valor recibido
+        estado |= (unsigned char) (a&15);
+        
+        
+        switch(estado&15){
+            case 0:
+                printf("Saliendo del programa... \n");
+                estado |= 32;
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                r = ejecutar(estado,a,b);
+            case 6:
+                a = leeValor("Seleccione el nuevo formato (0 - binario, 1 - hexadecimal): ", "Ese valor es válido\n",0,1);
+                if(a){
+                   estado |= 64;
+                   printf("Formato cambiado a hexadecimal\n");
+                }else{
+                   estado &= ~64;
+                   printf("Formato cambiado a binario\n");
+                }
+                break;
+            case 7:
+                estado |= 128;
+                printf("Memoria habilitada\n");
+                break;
+            case 8:
+                estado &= ~128;
+                printf("Memoria deshabilitada\n");
+                break;
+            case 9:
+                printf("Borrando el contenido de la memoria...\n");
+                if(borrarMemoria(memoria) == 0) printf("Memoria borrada\n");
+                else printf("Error al borrar memoria\n");
+                break;
+            case 10:
+                printf("El contenido de la memoria es:\n");
+                imprimirMemoria(memoria);
+                break;
+            default:
+                break;
+        }
+        
+        // En caso de estar habilitada la memoria, guardamos la operación realizada
+        if(estado&128) guardar(estado,a,b,r);
+        
+    }
+    return 0;
 }
