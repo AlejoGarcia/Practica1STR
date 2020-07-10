03ec066afa981ad982a674173d2d24f714200923
Alejo García Galán <alejogarciagalan@gmail.com>
2020-07-10 14:02:34 +0200

    Cambios sustanciales realizados, por eso creo rama nueva

--- auxiliar.c
+++ auxiliar.c
@@ -1,23 +1,111 @@
-// Condigo fuente del fichero auxiliar.h
+//==========================================================================================||
+//                                                                                          ||
+//      Header que contiene las funciones auxiliares para el programa de la calculadora     ||
+//                                                                                          ||
+//==========================================================================================||
 
-// Header que contiene las funciones auxiliares para el programa de la calculadora
 
-// Comprobamos que no se haya importado antes para no importarlo dos veces
-#ifndf AUXILIAR_H
-#define AUXILIAR_H
+#include <string.h>
+#include "auxiliar.h"
 
-// Cadenas que se emplearán en la interfaz de la calculadora de forma recurrente
-#define TEXTOMENU "Calcula"
-#define BARRA ------------------------------------------------------
 
-//----------------------------------------------------------------------------------
-// Estructura para implementar la memoria de la calculadora (dada por el ejercicio)
-//----------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------
+// Funciones para descomponer la informacion de la variable estado
+//-----------------------------------------------------------------
+
+// Devuelve el estado de la memoria (habilitada 1, deshabilitada 0)
+char estadoMemoria(char _cod){
+    return (_cod&128)>>7;
+}
+
+char estadoFormato(char _cod){
+    return (_cod&64)>>6;
+}
+
+char estadoFormato(char _cod){
+    return (_cod&32)>>5;
+}
+
+char estadoOperacion(char _cod){
+    return _cod&15;
+}
+
+
+//-----------------------------------------------
+// Funciones de entrada/salida de la calculadora
+//-----------------------------------------------
+
+// Muestra la cabecera del programa (BARRA|TEXTOTITULO|\n|BARRA)
+void mostrarTitulo(){
+    printf(BARRA);
+    printf(strcat(TEXTOTITULO,"\n"));
+    printf(BARRA);
+}   
+
+// Muestra el menu inicial del programa (\n\n|TEXTOMENU|BARRA)
+void mostrarMenu(){
+    print("\n\n")
+}     
+
+// Lee un valor numérico del teclado entre dos cotas dadas
+int leeValor(char* msg, char* err, int min, int max){
+    int r;          
+    printf(msg);
+    scanf("%d",&r);
+    while((r < min)||(r > max)){
+        printf(err);
+        printf(msg);
+        scanf("%d",&r);
+    }
+    return r;
+}   
+                                        
+// Función que devuelve el nombre de la operación
+void nombreOp(char _cod, char** nombre){
+    strcpy(*nombre, opEnum[estadoOperacion(_cod)]);
+}    
+
+
 
 //-------------------------------------------------------------
 // Funciones de ejecución de las operaciones de la calculadora
 //-------------------------------------------------------------
 
+// Funcion empleada para ejecutar operaciones binarias
+int ejecutar(char _cod, int _a, int _b){
+    int r;
+    switch(estadoOperacion(_cod)){
+        case 1:
+            r = _a | _b;
+            break;
+        case 2:
+            r = _a & _b;
+            break;
+        case 3:
+            r = _a ^ _b;
+            break;
+        case 4:
+            r = _a << _b;
+            break;
+        case 5:
+            r = _a >> _b;
+            break;
+        default:
+            printf("\n***ERROR: funcion ejecutar con código erroneo.\n")
+    }
+    return r;
+}
+
+void aBinario(int x, char** bin){
+    int i, m;
+    m = 1;
+    for (i = 0; i < 9; i++){
+        if(x&m) bin[8 - i] = '1';
+        else bin[8 - i] = '0';
+        m <<= 1;
+    }
+}
 
 
 
@@ -34,9 +122,50 @@ de cada operación). Todos los datos numericos se guardarán en el formato en el
 
 */
 
+// Guarda un elemento
+void guarda(nodo** _mem, int _x, char _f){
+    nodo * aux = *_mem;
+    if(aux == NULL){
+        aux = (nodo*) malloc(sizeof(nodo));
+    } else{
+        while(aux->siguiente != NULL) aux = aux->siguiente;
+        aux->siguiente = (nodo*) malloc(sizeof(nodo));
+        aux = aux->siguiente;
+    }
+    aux->siguiente = NULL;
+    switch(_f){
+        case 0:
+            
+        case 1:
+        case 2:
+            aux->contenido.operacion = estadoOperacion((char) _x);
+        default:
+            printf("\n***ERROR: guardaElemento, formato incorrecto.\n");
+            break;
+    }
+}
 
-void guarda(nodo**);    // Guarda un elemento completo en la calculadora del tipo:
-                        // [operación] [argumento/s(opcional)]
+// Guarda una operacion
+void guarda(nodo** _mem, char _cod, int _a, int _b,int _r){
+    char formato = estadoFormato(_cod);
+    guardaElemento(_mem,(int) _cod, 2);
+    switch(_cod&15){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            guardaElemento(_mem,_a,formato);
+            guardaElemento(_mem,_b,formato);
+            guardaElemento(_mem,_r,formato);
+            break;
+        case 6:
+            guardaElemento(_mem,_a,1);
+            break;
+        default:
+            break;
+    }
+}
 
 void borra(nodo**);     // Función auxiliar para borraMemoria(), borra el último que se haya guardado en memoria
 
@@ -49,6 +178,7 @@ void borraMemoria(nodo**);  // Borra el contenido de la memoria al completo deja
 
 void imprimeMemoria(nodo*); // Imprime de forma consecutiva todos los elementos contenidos en memoria hasta la ultima
                             // entrada (sig == NULL). En caso de recibir memoria vacia escribe la cadena "MEMORIA VACIA"
+
 
                            
 #endif // Se cierra el if del preprocesador que comprueba si esta libreria ya estaba cargada
--- auxiliar.h
+++ auxiliar.h
@@ -1,4 +1,8 @@
-// Header que contiene las funciones auxiliares para el programa de la calculadora
+//==========================================================================================||
+//                                                                                          ||
+//      Header que contiene las funciones auxiliares para el programa de la calculadora     ||
+//                                                                                          ||
+//==========================================================================================||
 
 
 // Comprobamos que no se haya importado antes para no importarlo dos veces
@@ -8,11 +12,11 @@
 
 
 
-// Cadenas que se emplearán en la interfaz de la calculadora de forma recurrente
+// Cadenas que se emplearán en la interfaz de la calculadora de forma recurrente (formatear de ser necesario)
 
-#define TEXTOMENU "Calculadora Binaria Simple\n"
-#define TEXTOMENU "1.- OR lógico\n2.- AND lógico\n3.- XOR lógico\n4.- << (desplazamiento a la izquierda)\n5.- >> (desplazamiento a la derecha)\n6.- Cambio de formato (0→ binario, 1→ hexadecimal)\n7.- Habilitar memoria\n8.- Deshabilitar memoria\n9.- Borrar memoria\n10.- Mostrar el contenido de la memoria\n0.- SALIR"
-#define BARRA ------------------------------------------------------
+#define TEXTOTITULO "Calculadora Binaria Simple\n"
+#define BARRA "------------------------------------------------------\n"
+enum opEnum = {"SALIR", "OR lógico", "AND lógico", "XOR lógico", "<< (desplazamiento a la izquierda)", ">> (desplazamiento a la derecha)", "Cambio de formato (0→ binario, 1→ hexadecimal)", "Habilitar memoria", "Deshabilitar memoria", "Borrar memoria", "Mostrar el contenido de la memoria"} // Cadenas con los nombres de las distintas operaciones posibles
 
 
 
@@ -31,17 +35,35 @@ typedef struct node{
 
 
 
+//-----------------------------------------------------------------
+// Funciones para descomponer la informacion de la variable estado
+//-----------------------------------------------------------------
+
+char estadoMemoria(char);
+
+char estadoFormato(char);
+
+char estadoSalir(char);
+
+char estadoOperacion(char);
+
+
+
 //-----------------------------------------------
 // Funciones de entrada/salida de la calculadora
 //-----------------------------------------------
 
-void mostrarTitulo();   // Muestra la cabecera del programa
+void mostrarTitulo();   // Muestra la cabecera del programa (BARRA|TEXTOTITULO|BARRA)
 
-void mostrarMenu();     // Muestra el menu inicial del programa
+void mostrarMenu();     // Muestra el menu inicial del programa (\n\n|TEXTOMENU|BARRA)
 
 int leeValor(char*, char*, int, int);   // Lee un valor numérico del teclado entre dos cotas dadas pidiendolo 
                                         // con el mensaje indicado, en caso de salirse del margen imprime el
                                         // mensaje de error que se le indica
+
+void nombreOp(char, char**);    // Función que devuelve el nombre de la operación condificada dentro de los bits
+                                // 0,1,2 y 3 en función de su código del menú escribiendolo sobre un char[30]
+                                // siguiendo el formato de la estructura de almacenamiento y los nombres dados
 
 
 
@@ -49,9 +71,12 @@ int leeValor(char*, char*, int, int);   // Lee un valor numérico del teclado en
 // Funciones de ejecución de las operaciones de la calculadora
 //-------------------------------------------------------------
 
-int ejecutar(char, int, int);
+int ejecutar(char, int, int);   // Funcion empleada para ejecutar todas las operaciones binarias (con dos operandos)
+                                // de la calculadora
 
-void aBinario(int,**char);
+void aBinario(int, char**);
+
+
 
 
 //-----------------------------------------------------
@@ -66,8 +91,8 @@ de cada operación). Todos los datos numericos se guardarán en el formato en el
 
 */
 
-void guarda(nodo**,char,int,int,int);    // Guarda un elemento completo en la calculadora del tipo:
-                        // [operación] [argumento/s(opcional)]
+void guarda(nodo**,char,int,int,int);   // Guarda un elemento completo en la calculadora del tipo:
+                                        // [operación] [argumento/s(opcional)]
 
 void borra(nodo**);     // Función auxiliar para borraMemoria(), borra el último que se haya guardado en memoria
 
